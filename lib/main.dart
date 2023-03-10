d17f2b203c737685b531b523832845b7255f141f
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-03-10 11:15:13 +0800

    SettingUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:intro_screen/screens/intro/intro.dart';
+
+main() => runApp(const App());
+
+class App extends StatelessWidget {
+
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const Intro(),
+      },
+    );
+  }
+}
